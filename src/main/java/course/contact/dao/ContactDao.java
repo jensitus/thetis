7348a6d1db1dfdb9d7836b885eb0a705f7699161
jens <jens@service-b.org>
2014-03-05 11:29:55 +0100

    show user

--- src/main/java/course/contact/dao/ContactDao.java
+++ src/main/java/course/contact/dao/ContactDao.java
@@ -8,5 +8,6 @@ public interface ContactDao {
     public boolean createContact(int readerId, int toReadId);
     public boolean readConnectedUsers(int userReaderId, int userToReadId);
     public boolean deleteContact(int readerId, int toReadId);
-    public List<User> connectedUserList(int userReaderId);
+    public List<User> connectedWith(int userReaderId);
+    public List<User> connectedBy(int userToReadId);
 }
