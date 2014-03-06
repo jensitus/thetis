b552487f2e1eda712418a2616dcc57037260f532
Florian Kleedorfer <florian.kleedorfer@austria.fm>
2014-03-06 23:41:59 +0100

    added logging with logback

--- src/main/java/course/dataaccess/MysqlDaoFactory.java
+++ src/main/java/course/dataaccess/MysqlDaoFactory.java
@@ -19,6 +19,8 @@ package course.dataaccess;
 
 import course.user.dao.CrudUserDao;
 import course.user.dao.UserDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
 import java.io.IOException;
@@ -26,12 +28,14 @@ import java.util.Properties;
 
 public class MysqlDaoFactory
 {
-  private static MysqlDaoFactory instance = new MysqlDaoFactory();
+  private final Logger logger = LoggerFactory.getLogger(getClass());
+  private static final MysqlDaoFactory instance = new MysqlDaoFactory();
   private javax.sql.DataSource dataSource;
   private UserDao userDao;
 
   private MysqlDaoFactory()
   {
+    if (instance != null) throw new IllegalStateException("This is a singleton, don't mess with it!");
     try {
       setup();
     } catch (IOException e) {
@@ -50,17 +54,21 @@ public class MysqlDaoFactory
 
   private void setup() throws IOException
   {
+    logger.info("setting up mysql dao factory");
     //setup data source
     this.dataSource = initDataSource();
     //setup daos
+    logger.debug("instantiating daos");
     this.userDao = new CrudUserDao(this.dataSource);
-
+    logger.debug("finished setting up mysql dao factory");
   }
 
   private DataSource initDataSource() throws IOException
   {
+    logger.info("loading database.properties");
     Properties properties = new Properties();
     properties.load(DataSource.class.getClassLoader().getResourceAsStream("database.properties"));
+    logger.info("creating datasource");
     com.mysql.jdbc.jdbc2.optional.MysqlDataSource ds
         = new com.mysql.jdbc.jdbc2.optional.MysqlDataSource();
     String jdbcUrl = properties.getProperty("jdbc.url");
